d4890af28dd386becdcfe239320b91d8a8e28f29
khadijahnabil <nabilkhadijah7@gmail.com>
2025-03-29 22:22:09 +0100

    added interactive icons, new landing page, and new app icon

---
+++ src/ui/assets/game/interactive/interactive-index.ts
@@ -0,0 +1,5 @@
+import HappinessIcon from "./happiness.png";
+import LoveIcon from "./love.png";
+import PlayIcon from "./play.png";
+
+export { HappinessIcon, LoveIcon, PlayIcon };
--- src/ui/pages/game.tsx
+++ src/ui/pages/game.tsx
@@ -2,19 +2,27 @@ import { useEffect, useRef, useState } from "react";
 import GameBg from "../assets/game/game-bg.png";
 import Character1 from "../assets/game/characters/chara1.png";
 import { setupCanvasWithImage } from "../../helpers/canvasHelper";
+import { PlayIcon, HappinessIcon, LoveIcon } from "../assets/game/interactive/interactive-index";
 
 export default function Game() {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const characterCanvasRef = useRef<HTMLCanvasElement | null>(null);
   const [isHovered, setIsHovered] = useState<boolean>(false);
 
+  const interactiveIcons = [HappinessIcon, LoveIcon, PlayIcon];
+
   useEffect(() => {
-    return setupCanvasWithImage(canvasRef.current, Character1, setIsHovered);
+    setupCanvasWithImage(characterCanvasRef.current, Character1, setIsHovered);
   }, []);
 
   return (
     <div className="game-container">
       <img src={GameBg} alt="Game Background" className="game-bg-image" />
-      <canvas ref={canvasRef} className={`character-canvas ${isHovered ? "hovered" : ""}`} />
+      <canvas ref={characterCanvasRef} className={`character-canvas ${isHovered ? "hovered" : ""}`} />
+      <div className="interactive-icons-container">
+        {interactiveIcons.map((icon, index) => (
+          <img key={index} src={icon} className="interactive-icon" />
+        ))}
+      </div>
     </div>
   );
 }
